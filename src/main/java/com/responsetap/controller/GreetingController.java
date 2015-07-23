76211b8db3640fbdc6ce0558e04eac453af43db5
James Heggs <james.heggs@responsetap.com>
2015-07-23 11:43:58 +0100

    Specify specific GET request for greeting controller to ensure it is clear during tutorial

--- src/main/java/com/responsetap/controller/GreetingController.java
+++ src/main/java/com/responsetap/controller/GreetingController.java
@@ -3,12 +3,13 @@ package com.responsetap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping(method= RequestMethod.GET, value="/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";

6be61b3cdda8d02a20a1f03ae401d53e3e09a6d9
James Heggs <james.heggs@responsetap.com>
2015-07-23 11:42:45 +0100

    Use correct mapping for GET request

--- src/main/java/com/responsetap/controller/PhonecallController.java
+++ src/main/java/com/responsetap/controller/PhonecallController.java
@@ -2,12 +2,13 @@ package com.responsetap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class PhonecallController {
 
-    @RequestMapping("/phonecall")
+    @RequestMapping(value="/phonecall", method = RequestMethod.GET)
     public String showPhoneCallPage() {
         return "phonecall";
     }

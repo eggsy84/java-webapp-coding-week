ac21356772f885f6e5613971275c4590b024e784
James Heggs <james.heggs@responsetap.com>
2015-07-24 15:28:50 +0100

    Begin validation annotations

--- src/main/java/com/responsetap/model/PhonecallForm.java
+++ src/main/java/com/responsetap/model/PhonecallForm.java
@@ -1,8 +1,15 @@
 package com.responsetap.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Size;
+
 public class PhonecallForm {
 
+    @Size(min=11, max=11, message = "Please make sure your number is 11 digits")
     private String number;
+
+    @NotBlank(message = "Please give me a message")
     private String text;
 
     public String getNumber() {

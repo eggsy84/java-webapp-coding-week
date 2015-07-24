4ecc0ceaf926b6aef9390a6f39db89fac2a51686
James Heggs <james.heggs@responsetap.com>
2015-07-24 15:18:47 +0100

    Form submission implementation

--- src/main/java/com/responsetap/controller/PhonecallController.java
+++ src/main/java/com/responsetap/controller/PhonecallController.java
@@ -1,6 +1,9 @@
 package com.responsetap.controller;
 
+import com.responsetap.model.PhonecallForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,7 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PhonecallController {
 
     @RequestMapping(value="/phonecall", method = RequestMethod.GET)
-    public String showPhoneCallPage() {
+    public String showPhoneCallPage(Model model) {
+
+        model.addAttribute("phonecallForm", new PhonecallForm());
+
+        return "phonecall";
+    }
+
+    @RequestMapping(value="/phonecall", method = RequestMethod.POST)
+    public String showPhoneCallPage(@ModelAttribute PhonecallForm phonecallForm, Model model) {
         return "phonecall";
     }
 }
---
+++ src/main/java/com/responsetap/model/PhonecallForm.java
@@ -0,0 +1,23 @@
+package com.responsetap.model;
+
+public class PhonecallForm {
+
+    private String number;
+    private String text;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}

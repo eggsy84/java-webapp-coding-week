abfe425ed64b2e0ace2b94844d256a01e6a331be
James Heggs <james.heggs@responsetap.com>
2015-07-24 16:03:27 +0100

    Add validation to form submission

--- src/main/java/com/responsetap/controller/PhonecallController.java
+++ src/main/java/com/responsetap/controller/PhonecallController.java
@@ -3,9 +3,12 @@ package com.responsetap.controller;
 import com.responsetap.model.PhonecallForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -20,8 +23,14 @@ public class PhonecallController {
     }
 
     @RequestMapping(value="/phonecall", method = RequestMethod.POST)
-    public String handleSubmitPhonecallPage(@ModelAttribute PhonecallForm phonecallForm, Model model) {
-        model.addAttribute("phonecallForm", phonecallForm);
-        return "result";
+    public String handleSubmitPhonecallPage(Model model, @Valid PhonecallForm phonecallForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "phonecall";
+        }
+        else {
+            model.addAttribute("phonecallForm", phonecallForm);
+            return "result";
+        }
     }
 }

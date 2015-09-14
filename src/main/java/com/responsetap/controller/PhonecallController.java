d3fb26bbc35b36395192c4c5236c9bea7b335554
eggsy84 <eggsy@eggsylife.co.uk>
2015-09-14 16:07:53 +0100

    Introduce concept of looping

--- src/main/java/com/responsetap/controller/PhonecallController.java
+++ src/main/java/com/responsetap/controller/PhonecallController.java
@@ -1,6 +1,7 @@
 package com.responsetap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,7 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PhonecallController {
 
     @RequestMapping(value="/phonecall", method = RequestMethod.GET)
-    public String showPhoneCallPage() {
+    public String showPhoneCallPage(ModelMap modelMap) {
+
+
+        int counterValue = 0;
+
+        for( int i = 0; i < 100; i++) {
+            counterValue = counterValue + 1; //counterValue++
+        }
+
+        String message = String.format("You looped %d times", counterValue);
+        modelMap.addAttribute("loopCounterValue", message);
+
         return "phonecall";
     }
 }
